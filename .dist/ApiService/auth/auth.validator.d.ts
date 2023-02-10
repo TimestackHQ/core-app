23b72c369eb48db44340106f3c8030cce3aaaf2b
Achraf Ghellach <39340160+ghellach@users.noreply.github.com>
2023-02-09 22:44:22 -0500

    Delete .dist directory

--- .dist/ApiService/auth/auth.validator.d.ts
+++
@@ -1,4 +0,0 @@
-import * as Joi from "joi";
-export declare const loginValidator: (body: unknown) => Joi.ValidationResult;
-export declare const confirmLoginValidator: (body: unknown) => Joi.ValidationResult;
-export declare const registerValidator: (body: unknown) => Joi.ValidationResult;
