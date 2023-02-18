823a4d23d53e3d69a7a7b6540f3155d6117cf3b9
Achraf Ghellach <a.ghellach@gmail.com>
2023-02-18 16:12:32 -0500

    main page update

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -7,6 +7,7 @@ import store from "../store";
 import AuthCheck from "../components/AuthCheck";
 import Script from "next/script";
 import 'react-big-calendar/lib/sass/styles.scss';
+import * as React from "react";
 
 
 function MyApp({ Component, pageProps }) {
@@ -21,6 +22,7 @@ function MyApp({ Component, pageProps }) {
           <Script src="https://kit.fontawesome.com/496369c1d1.js"/>
           <Head>
               <title>Timestack</title>
+              <link href="images/favicon.png" rel="shortcut icon" type="image/x-icon" />
               <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
                     rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
                     crossOrigin="anonymous"/>
