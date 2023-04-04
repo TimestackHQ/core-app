dfe000815d8e7f87458478c4c822ce95bcccf62c
Achraf Ghellach <a.ghellach@gmail.com>
2023-04-03 23:36:14 -0400

    update

--- ios/App.js
+++ ios/App.js
@@ -110,7 +110,7 @@ function ErrorScreen() {
 export default function App() {
 
     useEffect( () => {
-        const bundleVersion = "0.22.5";
+        const bundleVersion = "0.22.6";
         axios.get(frontendUrl+"/api/bundle").then(async (_res) => {
             if(_res.data.bundleVersion !== bundleVersion) {
                 await Updates.fetchUpdateAsync();
