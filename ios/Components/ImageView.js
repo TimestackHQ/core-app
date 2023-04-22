26dc100346e158bacd989981acee863a29b328b7
Achraf Ghellach <a.ghellach@gmail.com>
2023-04-22 16:09:30 -0400

    updates

--- ios/Components/ImageView.js
+++ ios/Components/ImageView.js
@@ -1,13 +1,26 @@
-import { Image, Dimensions } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Image, Dimensions, View, Text } from 'react-native';
+import ErrorBoundary from 'react-native-error-boundary';
 import Pinchable from 'react-native-pinchable';
 
 const { width } = Dimensions.get('window');
 
-export default function ImageView ({item}) {
-    return <Pinchable maximumZoomScale={5}>
-    <Image
+const ImageViewCore = ({item}) => {
+    return <Image
         source={{ uri: item?.storageLocation }}
         style={{ width, height: "100%", resizeMode: "contain" }}
     />
-</Pinchable>
+}
+
+export default function ImageView ({item}) {
+
+    function imageViewer () {
+        return <ImageViewCore item={item}/>
+    }
+
+    return <ErrorBoundary FallbackComponent={() => imageViewer()}>
+        <Pinchable maximumZoomScale={5}>
+            {imageViewer()}
+        </Pinchable>
+    </ErrorBoundary>
 }
