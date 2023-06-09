b58680c73b369d72714d45b8496cd78439926d0a
Achraf Ghellach <a.ghellach@gmail.com>
2023-06-09 13:08:32 -0400

    update

--- web/pages/api/bundle.js
+++ web/pages/api/bundle.js
@@ -2,13 +2,13 @@ export default function handler(req, res) {
 	res.status(200).json({
 		bundleVersion: "0.23.2",
 		clientVersion: {
-			ios: "0.23.3",
+			ios: "0.23.2",
 			android: "0.23.2"
 		},
 		backwardsCompatibleClients: {
 			ios: [
 
-				"0.23.2",
+				"0.23.3",
 
 			],
 			android: [
